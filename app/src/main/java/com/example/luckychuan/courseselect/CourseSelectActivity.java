a424b1e825ee8789a2d6db827d90403edb855fee
Luckychuan <luckychuan96@163.com>
2017-11-20 19:41:59 +0800

    实现toolbar

--- app/src/main/java/com/example/luckychuan/courseselect/CourseSelectActivity.java
+++ app/src/main/java/com/example/luckychuan/courseselect/CourseSelectActivity.java
@@ -1,13 +1,80 @@
 package com.example.luckychuan.courseselect;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * Created by Luckychuan on 2017/11/20.
+ */
 
 public class CourseSelectActivity extends AppCompatActivity {
+
+    private SearchView mSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_select);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.course_toolbar);
+        //设置我们的ToolBar
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_course_select_activity, menu);
+        MenuItem searchItem = menu.findItem(R.id.menu_search);
+        //通过MenuItem得到SearchView
+        mSearchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        mSearchView.setIconifiedByDefault(true);
+        mSearchView.setIconified(true);
+        mSearchView.setQueryHint("输入课类、教师名");
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                refreshData();
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_level:
+                refreshData();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void refreshData(){
+        Toast.makeText(this, "刷新数据", Toast.LENGTH_SHORT).show();
     }
 }
